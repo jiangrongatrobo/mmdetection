ad74d42cf55b5104c4984bf0cd7bfd5531acca66
Jiang, Rong <jiangrongatrobo@roborock.com>
2021-03-29 09:56:16 +0000

    sync

--- configs/baiguang34/fcos_r50_caffe_fpn_gn-head_1x_coco_baiguang34_640x480.py
+++ configs/baiguang34/fcos_r50_caffe_fpn_gn-head_1x_coco_baiguang34_640x480.py
@@ -58,3 +58,33 @@ data = dict(
 load_from = 'configs/baiguang34/fcos_r50_caffe_fpn_gn-head_1x_coco-821213aa.pth'
 
 total_epochs = 20
+
+#################### 640x480 ###################
+img_norm_cfg = dict(
+    mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(640, 480), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(640, 480),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+#################### 640x480 ###################
