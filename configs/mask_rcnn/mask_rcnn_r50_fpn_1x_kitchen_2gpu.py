538d6899995e07c819af29d47cfa47095fee09ad
Jiang, Rong <jiangrongatrobo@roborock.com>
2021-04-20 06:08:18 +0000

    sync

--- configs/mask_rcnn/mask_rcnn_r50_fpn_1x_kitchen_2gpu.py
+++ configs/mask_rcnn/mask_rcnn_r50_fpn_1x_kitchen_2gpu.py
@@ -34,3 +34,4 @@ model = dict(
 load_from = 'http://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_fpn_1x_coco/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth'
 log_config = dict(interval=1)
 # evaluation = dict(metric=['bbox'])
+total_epochs = 48
